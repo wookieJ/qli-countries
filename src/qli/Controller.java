b0700f861612cb5965004d31ad2cea96e911df9b
jandraszyk <jasiek2695@gmail.com>
2018-11-24 13:51:43 +0100

    Initial sketch of the GUI

---
+++ src/qli/Controller.java
@@ -0,0 +1,4 @@
+package qli;
+
+public class Controller {
+}
---
+++ src/qli/Main.java
@@ -0,0 +1,22 @@
+package qli;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Quality of Life EU");
+        primaryStage.setScene(new Scene(root, 1280, 720));
+        primaryStage.show();
+    }
+}
