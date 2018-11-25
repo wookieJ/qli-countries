82c8cd1ad4fc3ef60c90902b906d2116bf31c5cc
wookieJ <lukaszjedryczka.biuro@gmail.com>
2018-11-25 11:39:56 +0100

    Main path changed

---
+++ main.py
@@ -0,0 +1,13 @@
+from model.parameter import Parameter
+
+
+def run():
+    parameter = Parameter('health')
+    parameter.load_sub_params()
+    parameter.print_sub_params_values(print_label=True)
+    print('Raw values:')
+    print(parameter.get_values(2016))
+
+
+if __name__ == '__main__':
+    run()
